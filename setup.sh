d2378533892e0b1cae26ff7c8910fc3b40105856
Eric Carlson <e.carlson94@gmail.com>
2023-10-11 22:39:39 -0400

    force devbox global pull

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 curl -fsSL https://get.jetpack.io/devbox | bash
 
 # Setup devbox
-devbox global pull https://github.com/ecarlson94/devbox.git
+devbox global pull https://github.com/ecarlson94/devbox.git -f
 
 # Setup dotfiles
 devbox global run dotfiles
