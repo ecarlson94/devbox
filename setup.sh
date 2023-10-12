bbbe8ba46dc94f786c61231f48b0474233dadeb5
Eric Carlson <e.carlson94@gmail.com>
2023-10-11 22:34:05 -0400

    add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install devbox
+curl -fsSL https://get.jetpack.io/devbox | bash
+
+# Setup devbox
+devbox global pull https://github.com/ecarlson94/devbox.git
+
+# Setup dotfiles
+devbox global run dotfiles
