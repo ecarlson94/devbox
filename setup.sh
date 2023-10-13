d2a5660be796bb6b410d0169aa611a7aaeb3270c
Eric Carlson <e.carlson94@gmail.com>
2023-10-13 12:24:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,11 @@ curl -fsSL https://get.jetpack.io/devbox | bash -s -- -f
 # Setup devbox
 devbox global pull https://github.com/ecarlson94/devbox.git -f
 
-# Setup dotfiles
-devbox global run dotfiles
-
 # Source devbox
 eval "$(devbox global shellenv)"
+
+# Setup dotfiles
+devbox global run dotfiles
 
 # Configure ZSH as default shell
 sudo chsh -s $(which zsh) $(whoami)
