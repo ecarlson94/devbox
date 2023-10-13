65a768e2e860ee6504f2338e81721af7c1e3bba0
Eric Carlson <e.carlson94@gmail.com>
2023-10-13 13:22:06 -0400

    manually configure global devbox

--- setup.sh
+++ setup.sh
@@ -3,11 +3,20 @@
 # Install devbox
 curl -fsSL https://get.jetpack.io/devbox | bash -s -- -f
 
-# Setup devbox
-devbox global pull https://github.com/ecarlson94/devbox.git -f
+# Setup git
+devbox global add git
+eval "$(devbox global shellenv)"
+
+# Setup global devbox
+DEVBOX_GLOBAL_PATH=~/.local/share/devbox/global/default
+DEVBOX_TEMP_PATH=/tmp/devbox
+git clone https://github.com/ecarlson94/devbox.git $DEVBOX_TEMP_PATH
+cp /tmp/devbox/devbox.json $DEVBOX_GLOBAL_PATH
+cp /tmp/devbox/devbox.lock $DEVBOX_GLOBAL_PATH
+rm -rf $DEVBOX_TEMP_PATH
 
 # Source devbox
-eval "$(devbox global shellenv)"
+eval "$(devbox global shellenv --recompute)"
 
 # Setup dotfiles
 devbox global run dotfiles
